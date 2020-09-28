3da5374fd8f1fbd2e2ed1a9018310164976fa1bc
Yannick Kaupe <yannick.kaupe@gmail.com>
2020-09-28 17:54:25 +0200

    First problem

---
+++ Trivial/Bijele/bijele.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main() {
+	 int king, queen, rooks, bishops, knights, pawns = 0;
+	 scanf ("%d %d %d %d %d %d", &king, &queen, &rooks, &bishops, &knights, &pawns);
+
+	 printf("%d %d %d %d %d %d\n", 1-king, 1-queen, 2-rooks, 2-bishops, 2-knights, 8-pawns);
+
+	 return 0;
+ }
