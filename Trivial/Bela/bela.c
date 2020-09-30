06c7b7f76b7948af1a934ad9e8ac6561c06870c4
Yannick Kaupe <yannick.kaupe@gmail.com>
2020-09-30 21:44:08 +0200

    Added solution to Bela problem

--- Trivial/Bela/bela.c
+++ Trivial/Bela/bela.c
@@ -22,12 +22,13 @@ int lookup_card_value(char card, char suit, char trump){
         case 'T':
             value = 10;
             break;
-        case '9': 
+        case '9':
             if(suit == trump){
                 value = 14;
             } else {
                 value = 0;
             }
+	    break;
         //cases 8 and 7 seven are only mentioned for clarity.
         //value already has the proper value of 0
         case '8':
@@ -45,20 +46,22 @@ int main(){
     int num_hands = 0;
     char trump;
     char input[3];
+    int sum = 0;
     scanf("%d", &num_hands);
     scanf("%c",&trump);//get rid of the space
     scanf("%c",&trump);
 
     while(num_hands > 0){
 
-        scanf("%2s",input);
-        int value = lookup_card_value(input[0],input[1],trump);
-        printf("Value: %d\n",value);
+        for(int i = 0; i<4; i++){
+	    scanf("%2s",input);
+            int value = lookup_card_value(input[0],input[1],trump);
+	    sum += value;
+	}
         num_hands--;
     }
-
-    printf("Num hands: %d\n",num_hands);
-    printf("Trump: %c\n",trump);
+    
+    printf("%d\n",sum);
 
     return 0;
 }
